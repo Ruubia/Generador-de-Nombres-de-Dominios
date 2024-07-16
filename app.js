0d0d4c087e1c6c209fcd788c83bf582129dd983e
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-07-16 13:52:36 +0000

    añdi los dominios

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ var ext = [".com", ".net", ".us", ".io"];
 
 
 //bucle for
-
+function generarDominios() {
+    let dominios = [];
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
             for (let n = 0; n < ext.length; n++) {
-                console.log(
-                    pronoun[i] +
-                    adj[j] +
-                    noun[k].slice(0, 4) +
-                    "." +
-                    noun[k].slice(4, 6) +
-                    ext[n]
-                );
+                dominios.push(pronoun[i] + adj[j] + noun[k] + ext[n]);
             }
         }
     }
 }
+    return dominios;
+}
+let dominios = generarDominios();
+console.log(dominios); 
+let output = "";
+for (let i = 0; i < dominios.length; i++) {
+  output += `<p>${dominios[i]}</p>`;
+}
+document.getElementById("dominios").innerHTML = output;
+console.log("Dominios mostrados");
