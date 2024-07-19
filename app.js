d2e266a9923f288e27eaae38729651bb7014e59f
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-07-19 16:17:18 +0000

    cambie el titulo de la pagina, elimine un comentario y cambien las variables todas a ingles

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ var noun = ["jogger", "racoon"];
 var ext = [".com", ".net", ".us", ".io"];
 
 
-//bucle for
-function generarDominios() {
-    let dominios = [];
+
+function generateDomains() {
+    let domains = [];
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
             for (let n = 0; n < ext.length; n++) {
-                dominios.push(pronoun[i] + adj[j] + noun[k] + ext[n]);
+                domains.push(pronoun[i] + adj[j] + noun[k] + ext[n]);
             }
         }
     }
 }
-    return dominios;
+    return domains;
 }
-let dominios = generarDominios();
-console.log(dominios); 
-let output = "";
-for (let i = 0; i < dominios.length; i++) {
-  output += `<p>${dominios[i]}</p>`;
+let domains = generateDomains();
+console.log(domains); 
+let listDomains = "";
+for (let i = 0; i < domains.length; i++) {
+  listDomains += `<p>${domains[i]}</p>`;
 }
-document.getElementById("dominios").innerHTML = output;
+document.getElementById("dominios").innerHTML = listDomains;
 console.log("Dominios mostrados");
